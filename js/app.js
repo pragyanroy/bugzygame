145bceebc2bd0732dd10334777b2d1d3cc4f7450
Pragyan Roy <roy.pragyan@gmail.com>
2023-02-12 22:00:22 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ var Enemy = function(x, y) {
     this.y = y;
     this.s=this.x+this.y;
     
-    this.speed = Math.floor(Math.random() * .55) + 1;
+    this.speed = Math.floor(Math.random() * .055) + 1;
 };
 
 // Update the enemy's position, required method for game
